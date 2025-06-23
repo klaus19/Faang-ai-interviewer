a6fe86eb392bb7e11c783d414aba25ec940b2aca
klaus19 <tejaskhartude@gmail.com>
2025-06-23 15:35:42 +0530

    changed netlify file

--- backend/main.py
+++ backend/main.py
@@ -192,4 +192,5 @@ async def get_user_stats():
 if __name__ == "__main__":
     import uvicorn
     port = int(os.environ.get("PORT", 8080))
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
+
